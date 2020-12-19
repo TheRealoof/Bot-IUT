87478a89aa9bee5cfe7171676008476afbcbcd20
Clement DESKTOP <51529342+DAOgames@users.noreply.github.com>
2020-12-19 13:25:22 +0100

    ping

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ const Discord = require("discord.js")
 
 const client = new Discord.Client();
 
-//client.login(process.env.token);
+client.login(process.env.token);
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 
     // Set the client user's activity
-    client.user.setActivity('discord.js', { type: 'WATCHING' })
+    client.user.setActivity('discord.js', { type: 'PLAYING' })
         .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
         .catch(console.error);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+      msg.reply('pong');
+    }
 });
