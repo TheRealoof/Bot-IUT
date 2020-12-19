60233d4edef7c1f76c0bf4ea664f855159b9cca4
Clement DESKTOP <51529342+DAOgames@users.noreply.github.com>
2020-12-19 13:17:41 +0100

    stop

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const Discord = require("discord.js")
 
 const client = new Discord.Client();
 
-client.login(process.env.token);
+//client.login(process.env.token);
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+
+    // Set the client user's activity
+    client.user.setActivity('discord.js', { type: 'WATCHING' })
+        .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
+        .catch(console.error);
 });
