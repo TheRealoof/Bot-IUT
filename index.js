19f666990cc4971c3558cdf54ee1c2ce221d6d14
Clement DESKTOP <51529342+DAOgames@users.noreply.github.com>
2020-12-19 12:46:31 +0100

    log on ready

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const Discord = require("discord.js")
 
 const client = new Discord.Client();
 
-client.login(process.env.token)
+client.login(process.env.token);
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
