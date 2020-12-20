4138cef67c8b5993037f1cd35e49477cd9e8c07b
Clement DESKTOP <51529342+DAOgames@users.noreply.github.com>
2020-12-20 12:36:04 +0100

    Logs

--- index.js
+++ index.js
@@ -7,16 +7,39 @@ const client = new Discord.Client();
 client.login(process.env.token);
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    log(client.user.tag + " connecté !");
 
     // Set the client user's activity
     client.user.setActivity('discord.js', { type: 'PLAYING' })
-        .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
+        .then(presence => log("Activité définie : " + presence.activities[0].name))
         .catch(console.error);
 });
 
 client.on('message', msg => {
     if (msg.content === 'ping') {
-      msg.reply('pong');
+        msg.reply('pong');
     }
 });
+
+function getDate()
+{
+    var date = new Date(Date.now());
+    var seconds = "00" + date.getSeconds();
+    var minutes = "00"+ date.getMinutes();
+    var hours = "00" + date.getHours();
+    var days = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+    return days + "/" + month + "/" + year + " - " + hours.substr(hours.length-2,2) + ":" + minutes.substr(minutes.length-2,2) + ":" + seconds.substr(seconds.length-2,2);
+}
+
+function log(text)
+{
+    var message = "[" + getDate() + "] " + text;
+    console.log(message);
+    client.guilds.fetch("752503394450669668")
+        .then(guild => guild.channels.cache.get("773844590133641246").send(message)
+            .catch(console.error)
+        )
+        .catch(log);
+}
